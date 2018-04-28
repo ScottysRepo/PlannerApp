c61ba08e12df6b4ce543232160ca4735127dba22
Scotty <slseethoff1@buffs.wtamu.edu>
2018-04-28 00:53:06 -0500

    Changed UI layout. Added time picker and drop down menu.

--- app/src/main/java/com/example/sseethoff/plannerapp/CalendarActivity.java
+++ app/src/main/java/com/example/sseethoff/plannerapp/CalendarActivity.java
@@ -1,18 +1,31 @@
 package com.example.sseethoff.plannerapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CalendarActivity extends AppCompatActivity {
 
-   private static final String TAG = "CalendarActivity";
+    private static final String TAG = "MainActivity";
 
     private TextView thedate;
     private Button btngocalendar;
+    private Button btnAddActivity;
+    private EditText etAct;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +33,38 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         thedate = (TextView) findViewById(R.id.date);
         btngocalendar = (Button) findViewById(R.id.btngocalendar);
+        btnAddActivity = (Button) findViewById(R.id.btnAddActivity);
+        etAct = (EditText) findViewById(R.id.etAct);
+        spinner = (Spinner) findViewById(R.id.spinner);
+       final Context context = getApplicationContext();
+        final CharSequence text = "Event has been added!";
+        final int duration = Toast.LENGTH_SHORT;
 
         Intent incoming = getIntent();
         String date = incoming.getStringExtra("date");
         thedate.setText(date);
+        // Spinner click listener
+      //  spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Work");
+        categories.add("Shopping");
+        categories.add("School");
+        categories.add("Social");
+        categories.add("Travel");
+        categories.add("Other");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        etAct.setInputType(InputType.TYPE_CLASS_TEXT);
 
         btngocalendar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +73,15 @@ public class CalendarActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+        btnAddActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                String saved = etAct.getText().toString();
+                Intent intent = new Intent(CalendarActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });    }
 }
--- app/src/main/java/com/example/sseethoff/plannerapp/MainActivity.java
+++ app/src/main/java/com/example/sseethoff/plannerapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sseethoff.plannerapp;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,7 +11,7 @@ import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  static final String TAG = "MainActivity";
+    private  static final String TAG = "CalendarActivity";
     private CalendarView mCalendarView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
